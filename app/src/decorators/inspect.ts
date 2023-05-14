5d78d8e79779dc85f9ca8a0eb6db5ca1cea8dd36
Deyvison <dscsouza@gmail.com>
2023-05-14 12:57:08 -0400

    property escape implementado

---
+++ app/src/decorators/inspect.ts
@@ -0,0 +1,19 @@
+export function inspect(){
+    return function(
+        target:any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor 
+    ){
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args: any[]){
+            console.log(`-----= Método: ${propertyKey}`)
+            console.log(`-----= Argumentos: ${JSON.stringify(args)}`)
+            const retorno = metodoOriginal.apply(this, args);
+            console.log(`-----= Retorno: ${JSON.stringify(retorno)}`)
+            return retorno;
+        }
+        return descriptor;
+    }
+
+
+}
--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,13 +1,10 @@
-export function logarTempoDeExecucao(){
-
+export function logarTempoDeExecucao(emSegundos: boolean = false){
     /*
         EXPLICAÇÃO BÁSICA SOBRE OS PARÃMETROS DO DECORATOR
         target: any: Esse parâmetro representa o objeto alvo da decoração. No contexto de um decorator de classe, target se refere à própria classe sendo decorada. No contexto de um decorator de método, target se refere à função/método sendo decorado.
         propertyKey: string: Esse parâmetro representa a chave (nome) da propriedade/método sendo decorado. No caso de um decorator de classe, propertyKey será undefined, pois não está sendo aplicado diretamente a uma propriedade ou método específico.
         descriptor: PropertyDescriptor: Esse parâmetro contém informações sobre a propriedade/método sendo decorado. PropertyDescriptor é uma interface que define as propriedades de um descritor de propriedade. Ele contém informações como value (valor da propriedade/método), writable (se a propriedade/método pode ser escrita), enumerable (se a propriedade/método pode ser enumerada) e configurable (se a propriedade/método pode ser reconfigurada).
     */
-
-
     return function(
         target:any,
         propertyKey:string,
@@ -18,23 +15,19 @@ export function logarTempoDeExecucao(){
 
         //vamos alterar o método orignal, passando os argumentos originais
         descriptor.value = function(...args:Array<any>){
+            let divisor = 1;
+            let unidade = 'milesegundos';
+            if (emSegundos){
+                divisor = 1000;
+                unidade = 'segundos'
+            }
 
             const t1 = performance.now();
-
             const retorno = metodoOriginal.apply(this, args);
-
             const t2 = performance.now();
-            console.log(`Método: ${propertyKey}, tempo de execução ${(t2 - t1)/100}`);
-
+            console.log(`Método: ${propertyKey}, tempo de execução ${(t2 - t1)/divisor} ${unidade}`);
             retorno;
-
         }
         return descriptor;
     }
-
-    
-
-
-
-
 }
